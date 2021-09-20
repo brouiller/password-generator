650b60ad2788df4d8b1a7a8f67e42c631a7340b2
Father Shark <11543424+brouiller@users.noreply.github.com>
2021-09-20 18:18:54 -0400

    updated password validation to use regex

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 //primary function
 var generatePassword = function () {
+
 //arrays with different sets of characters
   var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   var upper = lower.map((value) => value.toUpperCase());
@@ -12,8 +13,8 @@ var generatePassword = function () {
 //array to hold user selected characters
   var charArray = [];
 
-//array to record which sets of characters the user selected
-  var userSelection = [false, false, false, false, 0];
+//array to record which sets of characters the user selected and how long they want the password to be
+  var userSelection = [];
 
 //array with question keywords
   var keyWords = ["lowercase", "UPPERCASE", "numeric", "special"];
@@ -22,78 +23,93 @@ var generatePassword = function () {
   var inceptionArray = [lower, upper, num, spec];
 
 //ask user for password length
-    var pwLength = window.prompt(
-      "Choose your password length. (min 8, max 128, no decimals)"
-    );
+  var pwLength = window.prompt(
+    "Choose your password length. (min 8, max 128, no decimals)"
+  );
 
 //check pwLength, if it's invalid, let the user know and return to the beginning
   if (pwLength === null) {
-
-//alert user if prompt is cancelled
-      return window.alert("You cancelled the prompt. Press Generate Password to retry.");
-    } else if (128 < pwLength || pwLength < 8 || pwLength % 1 != 0) {
-      window.alert("Can't you read?\nPlease enter a valid number.");
-      generatePassword();
+    //alert user if prompt is cancelled
+    window.alert(
+      "You cancelled the prompt. Press Generate Password to retry."
+    );
+    return "Press Generate Password to continue.";
+  } else if (128 < pwLength || pwLength < 8 || pwLength % 1 != 0) {
+    window.alert("Can't you read?\nPlease enter a valid number.");
+    generatePassword();
   } else {
 
 //if user input from the prompt passes validation, save value to keyWords array
-      keyWords[4] = pwLength;
+    userSelection[4] = pwLength;
 
-//for loop to ask which types to include and record selection
-      for (var i = 0; i < keyWords.length - 1; i++) {
-        var ask = window.confirm(
-          "Would you like to include " + keyWords[i] + " characters?"
-        );
-        if (ask) {
-          charArray.push.apply(charArray, inceptionArray[i]);
-          userSelection[i] = true;
-        };
+//for loop to ask which types to include and records selection
+    for (var i = 0; i < userSelection.length - 1; i++) {
+      var ask = window.confirm(
+        "Would you like to include " + keyWords[i] + " characters?"
+      );
+      if (ask) {
+        charArray.push.apply(charArray, inceptionArray[i]);
+        userSelection[i] = true;
       };
+    };
 
 //checks to make sure user selected at least one character type, otherwise starts it over
-      if (charArray.length === 0) {
-        window.alert(
-          "I can't generate a password from nothing. You have to choose at least one type.\nLet's try this again."
-        );
-        generatePassword();
-      };
+    if (charArray.length === 0) {
+      window.alert(
+        "I can't generate a password from nothing. You have to choose at least one type.\nLet's try this again."
+      );
+      generatePassword();
+    };
 
 //assembles the password by picking random elements from the character array until password length is achieved
+    var makePassword = function (z) {
       var pw = "";
-      var makePassword = function (z) {
-        for (var i = 0; i < z; i++) {
-          var index = Math.floor(Math.random() * charArray.length);
-          pw += charArray[index];
-        };
-
-//split password string into array for comparison
-        var pwToTestArray = pw.split("");
-
-//function to check if any characters in password are found in each selected character type array
-        var validatePassword = function (x, y) {
-          return y.some(function (v) {
-            return x.indexOf(v) >= 0;
-          });
-        };
-
-//loop through user selected arrays
-        for (var i = 0; userSelection.length - 1; i++) {
-          var y = true;
-          if (userSelection[i]) {
-            y = validatePassword(inceptionArray[i], pwToTestArray);
-
-//if password fails validation, generate a new one
-          } else if (!y) {
-            pw = '';
-            return makePassword(keyWords[4]);
-          } else {
-            return pw;
-          };
-        };
+      for (var i = 0; i < z; i++) {
+        var index = Math.floor(Math.random() * charArray.length);
+        pw += charArray[index];
       };
-      makePassword(keyWords[4]);
+
+//use regex to test password, regenerate password if it fails
+      var regexLow = /([a-z])/;
+      var regexUpp = /([A-Z])/;
+      var regexNum = /([0-9])/;
+      var regexSpe = /([!@#$%^&*])/;
+
+      if (userSelection[0]) {
+        lowMatch = regexLow.test(pw);
+      } else {
+        lowMatch = true;
+      };
+      if (userSelection[1]) {
+        uppMatch = regexUpp.test(pw);
+      } else {
+        uppMatch = true;
+      };
+      if (userSelection[2]) {
+        numMatch = regexNum.test(pw);
+      } else {
+        numMatch = true;
+      };
+      if (userSelection[3]) {
+        speMatch = regexSpe.test(pw);
+      } else {
+        speMatch = true;
+      };
+      if (lowMatch && uppMatch && numMatch && speMatch) {
+        return pw;
+      } else {
+        return false;
+      };
     };
-  return pw;
+  };
+
+//loop to run makePassword until it's valid
+  do {
+    var validPassword = makePassword(userSelection[4]);
+    if (validPassword) {
+      return validPassword;
+    };
+  } while (!validPassword);
 };
 
 // Write password to the #password input
